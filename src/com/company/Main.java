7fa69f8b8605d881f903ba13a292463032338fbb
Pierre Mariani <pierremariani@MacBook-Pro-de-admin-6.local>
2021-09-28 16:37:30 +0200

    Le menu des règles est fini

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,58 @@
 package com.company;
 
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        java.util.Scanner sc;
+        sc = new Scanner(System.in);
+        int selection = Integer.parseInt(sc.nextLine());
+
+        if (selection == 1) {
+            System.out.println("Le jeu va démarrer...");
+        } else if (selection == 2) {
+
+            System.out.println("                            ▓ Règles ▓ ");
+
+            System.out.println("                            ▓  ▲  ▲  ▓");
+
+            System.out.println("Pendant son tour un joueur peut déplacer son pion d’une case (verticalement ou\n" +
+                    "horizontalement), puis il détruit une case du plateau.\n" +
+                    "Le dernier joueur pouvant encore se déplacer gagne ");
+            System.out.println("Contraintes :\n" +
+                    "-Un joueur ne peut pas détruire une case occupée.\n" +
+                    "-Un joueur ne peut pas occuper une case détruite ou une case déjà occupée.\n" +
+                    "-Un joueur bloqué pendant un tour est déclaré perdant.\n" +
+
+                    " \nPour retourné au Menu tapez 1");
+
+
+            java.util.Scanner rt;
+            rt = new Scanner(System.in);
+            int retour = Integer.parseInt(rt.nextLine());
+
+            if (retour == 1) {
+
+            } else if (selection == 3) {
+                System.out.println("casse toi");
+            }
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
